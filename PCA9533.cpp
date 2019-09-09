50d33420b719b93e1599578e55e526f7acca89e8
NicoFR75 <40319466+NicoFR75@users.noreply.github.com>
2019-09-09 23:07:10 +0200

    Update PCA9533.cpp

--- PCA9533.cpp
+++ PCA9533.cpp
@@ -21,14 +21,14 @@ __asm volatile ("nop");
 
 #include "PCA9533.h"
 
-//    CONSTRUCTOR
+// CONSTRUCTOR
 PCA9533::PCA9533() {
 }
 
-//    DESTRUCTOR
+// DESTRUCTOR
 PCA9533::~PCA9533() {}
 
-//    PING (0 = Success, other = Error code) details in ReadMe
+// PING (0 = Success, other = Error code) details in ReadMe
 byte PCA9533::ping() {
   Wire.beginTransmission(DEV_ADDR);
   return Wire.endTransmission();
@@ -43,46 +43,68 @@ bool PCA9533::init() {
   return true;
 }
 
-//    SET REGISTER DATA
+// SET REGISTER DATA
 void PCA9533::setReg(reg_ptr_t regPtr, byte newSetting) {
-    if (regPtr > 0) {
-        initCall(regPtr);
-        Wire.write(newSetting);
-        endCall();
-    }
+  initCall(regPtr);
+  Wire.write(newSetting);
+  endCall();
 }
 
-//    INITIATE I2C COMMUNICATION
+// INITIATE I2C COMMUNICATION
 void PCA9533::initCall(reg_ptr_t regPtr) {
     Wire.beginTransmission(DEV_ADDR);
     Wire.write(regPtr);
 }
 
-//    SET MODE - Requires pin and mode to set.
-// 	  Parameters IOx & LED_MODE_x (See header file)
-void PCA9533::setMode(pin_t pin, led_out_mode_t newMode) {
+// SET MODE - Requires pin and mode to set.
+// Parameters IOx & LED_MODE_x (See header file)
+void PCA9533::setMODE(pin_t pin, led_out_mode_t newMode) {
     byte bit_mask;
     bit_mask = (3<<pin);
     port_setting = (port_setting & (~bit_mask)) | (newMode<<pin);
     setReg(REG_LED, port_setting);
 }
 
-//    SET MODE FOR ALL PINS :: Parameters ALL_ON | ALL_OFF
-void PCA9533::setMode(mode_t newMode) {
-    setReg(REG_LED, newMode);
-}
-
-//    SET PWM Registers :: Parameters REG_PWM0 | REG_PWM1
+// SET PWM Registers :: Parameters REG_PWM0 | REG_PWM1
 void PCA9533::setPWM(reg_ptr_t pwmPort, int pwmValue) {
 	setReg(pwmPort, pwmValue);
 }
 
-//    SET PSC Registers :: Parameters REG_PSC0 | REG_PSC1
+// SET PSC Registers :: Parameters REG_PSC0 | REG_PSC1
 void PCA9533::setPSC(reg_ptr_t pscPort, int pscValue) {
 	setReg(pscPort, pscValue);
 }
 
-//    STOP I2C COMMUNICATION
+// GET INPUTS Register :: Parameters IOx
+state_t PCA9533::getINPUT(pin_t pin) {
+  setReg(REG_INPUT, 0x00);
+  Wire.requestFrom((int)DEV_ADDR, 1, 1);
+  byte inputs = Wire.read();
+  state_t value = IO_LOW;
+  switch (pin) {
+    case IO0:
+      if(0x01 & inputs)
+        value = IO_HIGH;
+      break;
+    case IO1:      
+      if(0x02 & inputs)
+        value = IO_HIGH;
+      break;
+    case IO2:      
+      if(0x04 & inputs)
+        value = IO_HIGH;
+      break;
+    case IO3:      
+      if(0x08 & inputs)
+        value = IO_HIGH;
+      break;
+    default:
+      break;
+  }
+  return value;
+}
+
+// STOP I2C COMMUNICATION
 void PCA9533::endCall() {
     _comBuffer = Wire.endTransmission();
 }
