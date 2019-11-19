d588290ed272e428d625eb96dc416e968ce9a293
NicoFR75 <40319466+NicoFR75@users.noreply.github.com>
2019-11-20 00:09:17 +0100

    Update PCA9533.cpp
      - Added getReg method to read register
      - Added getAllReg method to read all register using AutoIncrement flag functionnality of the chip (can't start from INPUT reg, have to start from another register)
      - Added getINPUTS method to get GPIO values
      - Changed setMODE to read current register status before apply new port mode (instead of use port_setting which is hard coded at init stage)
      - Renamed some variables to make it more readable

--- PCA9533.cpp
+++ PCA9533.cpp
@@ -13,7 +13,15 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+
+  Edited by NicoFR75 (November 2019):
+  - Added getReg method to read register
+  - Added getAllReg method to read all register using AutoIncrement flag functionnality of the chip (can't start from INPUT reg, have to start from another register)
+  - Added getINPUTS method to get GPIO values
+  - Changed setMODE to read current register status before apply new port mode (instead of use port_setting which is hard coded at init stage)
+  - Renamed some variables to make it more readable
+  
+  */
 
 #if 1
 __asm volatile ("nop");
@@ -39,8 +47,16 @@ bool PCA9533::init() {
   setReg(REG_PSC1, 0x00);
   setReg(REG_PWM0, 0x00);
   setReg(REG_PWM1, 0x00);
-  setReg(REG_LED, port_setting);
+  setReg(REG_LED, port_init);
   return true;
+}
+// -------------------
+// PRIVATE
+// -------------------
+// INITIATE I2C COMMUNICATION
+void PCA9533::initCall(reg_ptr_t regPtr) {
+    Wire.beginTransmission(DEV_ADDR);
+    Wire.write(regPtr);
 }
 
 // SET REGISTER DATA
@@ -50,19 +66,46 @@ void PCA9533::setReg(reg_ptr_t regPtr, byte newSetting) {
   endCall();
 }
 
-// INITIATE I2C COMMUNICATION
-void PCA9533::initCall(reg_ptr_t regPtr) {
-    Wire.beginTransmission(DEV_ADDR);
-    Wire.write(regPtr);
+// GET REGISTER DATA
+byte PCA9533::getReg(reg_ptr_t regPtr) {
+  initCall(regPtr);
+  endCall();
+  Wire.requestFrom((int)DEV_ADDR, 1, 1);
+  byte received_byte = Wire.read();
+  endCall();
+  return received_byte;
 }
 
+// GET ALL REGISTERS DATA
+void PCA9533::getAllReg(byte (&values)[6]) {
+  initCall(REG_AUTO);
+  endCall();
+  Wire.requestFrom((int)DEV_ADDR, 6, 1);
+  int i = 0;
+  while(Wire.available())
+  { 
+    values[i] = Wire.read();
+    i++;
+  }
+  endCall();
+}
+
+// STOP I2C COMMUNICATION
+void PCA9533::endCall() {
+    _comBuffer = Wire.endTransmission();
+}
+
+// -------------------
+// PUBLIC
+// -------------------
 // SET MODE - Requires pin and mode to set.
 // Parameters IOx & LED_MODE_x (See header file)
 void PCA9533::setMODE(pin_t pin, led_out_mode_t newMode) {
     byte bit_mask;
     bit_mask = (3<<pin);
-    port_setting = (port_setting & (~bit_mask)) | (newMode<<pin);
-    setReg(REG_LED, port_setting);
+    byte led_setting = getReg(REG_LED);
+    byte new_setting = (led_setting & (~bit_mask)) | (newMode<<pin);
+    setReg(REG_LED, new_setting);
 }
 
 // SET PWM Registers :: Parameters REG_PWM0 | REG_PWM1
@@ -77,34 +120,7 @@ void PCA9533::setPSC(reg_ptr_t pscPort, int pscValue) {
 
 // GET INPUTS Register :: Parameters IOx
 state_t PCA9533::getINPUT(pin_t pin) {
-  setReg(REG_INPUT, 0x00);
-  Wire.requestFrom((int)DEV_ADDR, 1, 1);
-  byte inputs = Wire.read();
-  state_t value = IO_LOW;
-  switch (pin) {
-    case IO0:
-      if(0x01 & inputs)
-        value = IO_HIGH;
-      break;
-    case IO1:      
-      if(0x02 & inputs)
-        value = IO_HIGH;
-      break;
-    case IO2:      
-      if(0x04 & inputs)
-        value = IO_HIGH;
-      break;
-    case IO3:      
-      if(0x08 & inputs)
-        value = IO_HIGH;
-      break;
-    default:
-      break;
-  }
-  return value;
-}
-
-// STOP I2C COMMUNICATION
-void PCA9533::endCall() {
-    _comBuffer = Wire.endTransmission();
+  byte inputs = getReg(REG_INPUT);
+  byte mask = 1 << (pin/2);
+  return (state_t)((inputs & mask) >> (pin/2));
 }
